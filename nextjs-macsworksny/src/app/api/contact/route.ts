07d93b5cd6df93495792fe526eaa2e4a0384ac7c
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2025-09-10 16:28:08 -0400

    Update email form.

--- nextjs-macsworksny/src/app/api/contact/route.ts
+++ nextjs-macsworksny/src/app/api/contact/route.ts
@@ -1,4 +1,5 @@
-import sendgrid from "@sendgrid/mail";
+// import sendgrid from "@sendgrid/mail";
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import { NextResponse } from "next/server";
 
 interface EmailRequest {
@@ -31,15 +32,35 @@ export async function POST(request: Request): Promise<Response> {
     );
   }
 
-  const apiKey = process.env.SENDGRID_API_KEY;
-  if (!apiKey) {
-    console.error("SendGrid API key is missing");
+  // const apiKey = process.env.SENDGRID_API_KEY;
+  // if (!apiKey) {
+  //   console.error("SendGrid API key is missing");
+  //   return NextResponse.json(
+  //     { error: "SendGrid API key is missing" },
+  //     { status: 500 }
+  //   );
+  // }
+  // sendgrid.setApiKey(apiKey);
+
+  const region = process.env.AWS_REGION;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+  if (!region || !accessKeyId || !secretAccessKey) {
+    console.error("AWS SES configuration is missing");
     return NextResponse.json(
-      { error: "SendGrid API key is missing" },
+      { error: "AWS SES configuration is missing" },
       { status: 500 }
     );
   }
-  sendgrid.setApiKey(apiKey);
+
+  const sesClient = new SESClient({
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  });
 
   const makeEmail = (
     name: string,
@@ -66,19 +87,40 @@ export async function POST(request: Request): Promise<Response> {
     return msgString;
   };
 
-  const msg: sendgrid.MailDataRequired = {
-    to: ["casey.conlin@gmail.com", "MacsIronWorks@gmail.com"],
-    from: "info@parkcrestdesign.com",
-    subject: `Macs Iron Works Contact Form Submission - ${name}`,
-    text: message,
-    html: makeEmail(name, message, email, phone),
+  const params = {
+    Source: "info@parkcrestdesign.com", // Your verified sender email
+    Destination: {
+      ToAddresses: ["casey.conlin@gmail.com", "MacsIronWorks@gmail.com"], // Recipient email
+    },
+    Message: {
+      Subject: { Data: `Macs Iron Works Contact Form Submission from ${name}` },
+      // Body: { Text: { Data: `Email: ${email}\nMessage: ${message}` } },
+      Body: { Html: { Data: makeEmail(name, message, email, phone) } },
+    },
   };
 
   try {
-    await sendgrid.send(msg);
+    const command = new SendEmailCommand(params);
+    await sesClient.send(command);
     return NextResponse.json("Email sent successfully", { status: 200 });
   } catch (error) {
     console.error("Error sending email: ", error);
     return NextResponse.json("Error sending email", { status: 500 });
   }
 }
+
+//     to: ["casey.conlin@gmail.com", "MacsIronWorks@gmail.com"],
+//     from: "info@parkcrestdesign.com",
+//     subject: `Macs Iron Works Contact Form Submission - ${name}`,
+//     text: message,
+//     html: makeEmail(name, message, email, phone),
+//   };
+
+//   try {
+//     await sendgrid.send(msg);
+//     return NextResponse.json("Email sent successfully", { status: 200 });
+//   } catch (error) {
+//     console.error("Error sending email: ", error);
+//     return NextResponse.json("Error sending email", { status: 500 });
+//   }
+// }
